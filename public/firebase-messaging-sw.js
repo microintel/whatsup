038a71b06af475a7c8e543ce848c87e98ceb094a
Microintel <135629446+microintel@users.noreply.github.com>
2026-06-05 08:39:18 +0530

    Add files via upload

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,14 +6,14 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js"
 );
 
-firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyBkK16_d4R0VWeQCgOjH296cTbZNLxM-ts",
+  authDomain: "microintel-saraswati.firebaseapp.com",
+  projectId: "microintel-saraswati",
+  storageBucket: "microintel-saraswati.firebasestorage.app",
+  messagingSenderId: "42134813237",
+  appId: "1:42134813237:web:2ba582aa74b2a74c480f7c"
+};
 
 const messaging = firebase.messaging();
 
